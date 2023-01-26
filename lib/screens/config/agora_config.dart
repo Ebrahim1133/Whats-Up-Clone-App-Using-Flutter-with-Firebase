33383dd25fc680a60dd6529bdb734118df7e394f
Ebrahim Mohamed <em034578@gmail.com>
2023-01-26 16:43:12 +0200

    Full App

---
+++ lib/screens/config/agora_config.dart
@@ -0,0 +1,5 @@
+class AgoraConfig {
+  static String token = '';
+  static String appId = '9af76348feed46e8b58263be7148a6a5';
+  static String appcertificate = 'b9b553beb3e14f9081e6d11bfae5257f';
+}
